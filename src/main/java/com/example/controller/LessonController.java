484bf5f623fc323b93af77d2c59110127967d8fd
kotasuke55 <uverock5@gmail.com>
2023-05-30 16:31:17 +0900

    formの作成

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -3,14 +3,16 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.form.SampleForm;
 
 @Controller
 @RequestMapping("/lesson")
 public class LessonController {
-	
+
 	@GetMapping("/sample")
 	//@ResponseBody
 	public String sample(Model model) {
@@ -18,10 +20,17 @@ public class LessonController {
 		model.addAttribute("message", text);
 		return "index";
 	}
-	
-	@GetMapping("/request_test")
+
+	@PostMapping("/request_test")
 	@ResponseBody
-	public String getTest(@RequestParam("name") String name, @RequestParam("bloodType") String bloodType) {
-		return "名前: " + name + "<br> 血液型: " + bloodType;
+	public String postTest(SampleForm sampleForm) {
+		return "名前: " + sampleForm.getName() + "<br> 血液型: " + sampleForm.getBloodType();
 	}
+
+	@GetMapping("/form_test")
+    public String formTest(SampleForm sampleForm, Model model) {
+        model.addAttribute("sampleForm", sampleForm);
+        return "lesson/form_test";
+    }
+
 }
