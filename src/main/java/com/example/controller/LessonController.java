5a4e064cca28f66fd47f4e2ad3ed2010d4bf43a1
kotasuke55 <uverock5@gmail.com>
2023-05-30 15:47:48 +0900

    Controllerにgetの練習を追加

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/lesson")
@@ -15,5 +17,11 @@ public class LessonController {
 		String text = "Hello Spting Boot!!";
 		model.addAttribute("message", text);
 		return "index";
+	}
+	
+	@GetMapping("/request_test")
+	@ResponseBody
+	public String getTest(@RequestParam("name") String name, @RequestParam("bloodType") String bloodType) {
+		return "名前: " + name + "<br> 血液型: " + bloodType;
 	}
 }
